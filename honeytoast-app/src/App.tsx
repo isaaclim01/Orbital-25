a5d2f74dbe80c0cdd6cf664a40bb392e61516432
Strikerprv <chendongjun555@gmail.com>
2025-05-30 16:55:57 +0800

    Added sidebar using icons from React Icon, have not yet completed routing between pages

--- honeytoast-app/src/App.tsx
+++ honeytoast-app/src/App.tsx
@@ -1,13 +1,33 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Flight from './pages/Flight';
+import Hotel from './pages/Accomodation';
 
 function App() {
   return (
     <div className="App">
+      
+      <div className="navbar">
+        <BrowserRouter>
+            < Navbar />
+            <Routes>
+              <Route path="/" element={<Link to="/home"></Link>} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/flight" element={<Flight />} />
+              <Route path="/hotel" element={<Hotel />} />
+            </Routes>
+        </BrowserRouter>
+        
+      </div>
+
       <header> 
         HoneyToast
       </header>
+
       <div>
         Need to make the website
       </div>
--- honeytoast-app/src/components/Navbar.tsx
+++ honeytoast-app/src/components/Navbar.tsx
@@ -1,6 +1,44 @@
+import React, { useState } from 'react'
+import { FaBarsStaggered } from "react-icons/fa6";
+import { IoIosCloseCircleOutline } from "react-icons/io";
+import { Link } from 'react-router-dom';
+import { SidebarData } from './SidebarData';
+import './Navbar.css';
+
 function Navbar() {
+
+    const [sidebar, setSidebar] = useState(false);
+
+    const showSidebar = () => setSidebar(!sidebar);
+        
     return (
         <>
+        <div className="navbar">
+                <Link to="#">
+                    <FaBarsStaggered size="25" color="#1E8449" onClick={showSidebar}/>
+                </Link>
+        </div>
+        <div>
+            <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+                <ul className='nav-menu-items'>
+                    <li className='navbar-toggle'>
+                        <Link to="#" className='menu-bars'>
+                            <IoIosCloseCircleOutline size="25"/>
+                        </Link>
+                    </li>
+                {SidebarData.map((item, index) => {
+                        return (
+                            <li className={item.cName} key={index}>
+                                <Link to={item.path}>
+                                    {item.icon} 
+                                    <span>{item.title}</span>
+                                </Link>
+                            </li>
+                        )
+                })}
+                </ul>
+            </nav>
+        </div>
         </>
     )
 }
