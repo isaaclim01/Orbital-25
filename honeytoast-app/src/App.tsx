3ec04a07c6f9ab9826d12a1243b478f7bea5fadf
Strikerprv <chendongjun555@gmail.com>
2025-05-31 00:01:40 +0800

    Nav Bar fixed, and now can access different pages using it

--- honeytoast-app/src/App.tsx
+++ honeytoast-app/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Flight from './pages/Flight';
 import Accommodation from './pages/Accommodation';
@@ -10,30 +10,23 @@ import Calendar from './pages/Calendar';
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
+      <div className="App">
       
       <div className="navbar">
-        <BrowserRouter>
-            < Navbar />
+        < Navbar />
+      </div>
+      <div>
             <Routes>
-              <Route path="/" element={<Home />}  />
-              <Route path="/flight" element={<Flight />} />
+              <Route path="/"  element={<Home />}  />
+              <Route path="/flight"  element={<Flight />} />
               <Route path="/accommodation" element={<Accommodation />} />
               <Route path="/itinerary" element={<Itinerary />} />
               <Route path="/calendar" element={<Calendar />} />
-            </Routes>
-        </BrowserRouter>
-        
-      </div>
-
-      <header> 
-        HoneyToast
-      </header>
-
-      <div>
-        Need to make the website
+            </Routes>   
       </div>
     </div>
+     </BrowserRouter>
   );
 }
 
