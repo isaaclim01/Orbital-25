800fbc9ad7eeaebbe09010d6406e0cbb9ed9103c
Strikerprv <chendongjun555@gmail.com>
2025-06-15 23:37:24 +0700

    connected backend to frontend, fruit list in backend visible in Flight.tsx on frontend

--- honeytoast-app/src/pages/Flight.tsx
+++ honeytoast-app/src/pages/Flight.tsx
@@ -1,7 +1,40 @@
+import { useEffect, useState } from "react";
+import api from "../api";
+
+type Fruit = {
+  name: string;
+};
+
 function Flight() {
+
+  const [fruits, setFruits] = useState<Fruit[]>([]);
+
+  const fetchFruits = async () => {
+    try {
+      const response = await api.get('/fruits');
+      setFruits(response.data.fruits);
+    } catch (error) {
+      console.error("Error fetching fruits", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchFruits();
+  }, []);
+
+
   return (
-    <div>
-      Flight
+    // <div>
+    //   Flight
+    // </div>
+
+     <div>
+      <h2>Fruits List</h2>
+      <ul>
+        {fruits.map((fruit, index) => (
+          <li key={index}>{fruit.name}</li>
+        ))}
+      </ul>
     </div>
   )
 }
