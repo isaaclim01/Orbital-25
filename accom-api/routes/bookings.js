96b206ea40f9dd78892e51b457ed526e0ae2c4c0
isaaclim01 <e1366771@u.nus.edu>
2025-07-28 09:52:20 +0800

    included api changes

--- accom-api/routes/bookings.js
+++ accom-api/routes/bookings.js
@@ -1,16 +1,17 @@
 import express from "express";
-import { createBooking } from "../controllers/bookings.js";
+import { createBooking, deleteBooking, getAllBookings, getBooking, updateBooking } from "../controllers/bookings.js";
 
 const router = express.Router();
 
 //CREATE
 router.post("/", createBooking);
 //UPDATE
-
+router.put("/:id", updateBooking);
 //DELETE
-
+router.delete("/:id", deleteBooking);
 //GET
-
+router.get("/:id", getBooking);
 //GET ALL
+router.get("/", getAllBookings);
 
 export default router;
